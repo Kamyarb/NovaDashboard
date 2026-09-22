ee91667ed53a412769b36acb029812fcfb130a03
Kamyar Bagha <100142624+Kamyarb@users.noreply.github.com>
2026-09-22 21:16:25 +0330

    Clean unused imports

--- data_collector.py
+++ data_collector.py
@@ -4,7 +4,5 @@ import runpy
 import sys
 
 
-from collections import defaultdict
-
 sys.argv = [sys.argv[0], "--collect"] + sys.argv[1:]
 runpy.run_path(str(__import__("pathlib").Path(__file__).resolve().parent / "app.py"), run_name="__main__")
